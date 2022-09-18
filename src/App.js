57789f63fa40fe12d9bee15316b5b0ead92c0934
Alex <alexiampolski@optusnet.com.au>
2022-09-18 11:28:34 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+const url ="https://gist.githubusercontent.com/medibank-digital/a1fc81a93200a7b9d5f8b7eae0fac6f8/raw/de10a4fcf717e6c431e88c965072c784808fd6b2/people.json";
+let records;
+
+const getJsonData = async () => {
+  const responce = await fetch(url);
+  const jsonData = await responce.json();
+
+  records = jsonData.map((item) => {
+    let cats = [];
+    let headings = "";
+    let petsNumber = item.pets?.length || 0;
+
+    if (petsNumber > 0) {
+      headings = <h4>{item.gender}</h4>;
+      cats = item.pets.map(pet=>{return pet.type === "Cat" ?pet.name +"\n\r":''}).sort(); 
+
+    }
+
+   console.log(cats)
+
+    return (
+      <div>
+        {headings}
+        {cats}
+      </div>
+    );
+  });
+};
+
+
 
 function App() {
+  const [cats, setCats] = useState(0);
+  useEffect(() => {
+    getJsonData();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setCats(cats + 1)}>Get All Cats</button>
+        {records}
       </header>
     </div>
   );
